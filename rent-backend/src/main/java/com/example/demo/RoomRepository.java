35e5ed4c7dc5ca46ad33afc2dc7ed04dcfac87be
saiteja Gangireddi <98887511+SaitejaGangireddi@users.noreply.github.com>
2026-03-20 16:40:45 +0000

    Force update all rents to correct values

--- rent-backend/src/main/java/com/example/demo/RoomRepository.java
+++ rent-backend/src/main/java/com/example/demo/RoomRepository.java
@@ -2,8 +2,10 @@ package com.example.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
     // Standard CRUD operations are automatically included
+    Optional<Room> findByUnitNumber(String unitNumber);
 }
